0c277a068341b8cff60137915e3515420983355a
Eysteinn Örn Jónsson <eysi@live.com>
2025-08-07 18:42:11 +0000

    Revert "Update src/components/report-issue-button.tsx"
    This reverts commit 6951111bd4571d24962760c6d220a0af6c2f17f3.

--- src/components/report-issue-button.tsx
+++ src/components/report-issue-button.tsx
@@ -18,7 +18,7 @@ export function ReportIssueButton({
   }, []);
 
   const createIssueUrl = () => {
-    const baseUrl = GITHUB_ISSUE_BASE_URL;
+    const baseUrl = 'https://github.com/cps-acuo/travel-agent-docs/issues/new';
     const template = 'documentation-issue.yml';
     
     const params = new URLSearchParams({
