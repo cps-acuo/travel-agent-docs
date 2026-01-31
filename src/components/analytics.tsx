34842bc3a48fa52c9792f7b976ddbde8427c01df
jonmagg <jon@acuo.is>
2026-01-31 13:29:12 +0000

    docs: Add Google Analytics integration and update layout component

---
+++ src/components/analytics.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { usePathname, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+
+export default function Analytics() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const url = pathname + (searchParams?.toString() ? `?${searchParams}` : '');
+
+    window.gtag?.('config', 'G-DVNXWVQH7L', {
+      page_path: url,
+    });
+  }, [pathname, searchParams]);
+
+  return null;
+}
